9caf8b5177c4f07af7ad9c16d8ca551bc5617457
William Dueease <wdueease@altamusinc.com>
2025-01-31 14:12:31 -0500

    working sample, including compiled driver now, see test.py for usage of showing the lidar in open3d updating live

--- driver/python/src/test.py
+++ driver/python/src/test.py
@@ -1,16 +1,101 @@
 import limu_py
+import numpy as np
+import open3d as o3d
 
-foo = limu_py.ToF.tof320("10.10.31.180", "50660")
+frames = {}
 
+vis = o3d.visualization.Visualizer()
+geometry = o3d.geometry.PointCloud()
+update = False
+first = True
+
+def visualize(cloud):
+    geometries = []
+    geometries.append(cloud)
+    o3d.visualization.draw_geometries(geometries,
+                                width=1200,height=800,left=2000,top=100,
+                                window_name='Vis',
+                                zoom=0.75,
+                                front=[1,-1,1],
+                                lookat=[0,0,1],
+                                up=[0,0,1],
+                                point_show_normal=False,
+                                mesh_show_wireframe=False,
+                                mesh_show_back_face=False)
+    
 def handleFrame(frame):
-    print("got frame")
+    global vis
+    global geometry
+    global update
+    global first
+    if id(frame) not in frames:
+        frames[id(frame)] = frame
+        print("added frame")
 
-foo.subscribeFrame(handleFrame)
-foo.streamDistance()
+    # convert the frame list to an np array, and reshape to 8 things wide
+    xyz_rgb_np = np.array(frame.get_xyz_rgb()).reshape(frame.n_points, 8)
+    xyz = xyz_rgb_np[:,:3]
+    geometry.points = o3d.utility.Vector3dVector(xyz)
+    geometry.estimate_normals()
+    # pcd_xyz.paint_uniform_color([0, 0, 1])
+    if first:
+        vis.add_geometry(geometry)
+        first = False
+        print("Adding")
+    update = True
 
+def setParameters():
+    global tof
+
+    lensType = 0
+    frequencyModulation = 2
+    channel = 0
+    hdr_mode = 1
+    int0 = 1000
+    int1 = 1000 
+    int2 = 8000
+    intGr = 10000
+    minAmplitude = 60
+    lensCenterOffsetX = 0
+    lensCenterOffsetY = 0
+    roi_leftX = 0
+    roi_topY = 0
+    roi_rightX = 319
+    roi_bottomY = 239
+
+    tof.setModulation(frequencyModulation, channel)
+    tof.setMinAmplitude(minAmplitude)
+    tof.setIntegrationTime(int0, int1, int2, intGr)
+    tof.setHDRMode(hdr_mode)
+    tof.setRoi(roi_leftX, roi_topY, roi_rightX, roi_bottomY)
+    tof.setLensType(lensType)
+    tof.setLensCenter(lensCenterOffsetX, lensCenterOffsetY)
+    tof.setFilter(0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+
+
+tof = limu_py.ToF.tof320("10.10.31.180", "50660")
+setParameters()
+tof.streamDistance()
+vis.create_window(width=1200,height=800,left=2000,top=100)
+opt = vis.get_render_option()
+opt.point_size = 3
+opt.light_on = True
+opt.show_coordinate_frame = True
+opt.background_color = np.asarray([0.25, 0.25, 0.25])
+
+tof.subscribeFrame(handleFrame)
+# vis.run()
 while True:
-    user_input = input("Enter a command (or 'quit' to exit): ")
-    if user_input == "quit":
-        break
-    # Process the user input
-    print("You entered:", user_input)
+    if update is True:
+        vis.update_geometry(geometry)
+        vis.poll_events()
+        vis.update_renderer()
+        # print("updated")
+        update = False
+
+    # user_input = input("Enter a command (or 'quit' to exit): ")
+    # if user_input == "quit":
+    #     break
+    # # Process the user input
+    # print("You entered:", user_input) 
--- driver/python/src/wrapper.cpp
+++ driver/python/src/wrapper.cpp
@@ -3,11 +3,12 @@
 #include <pybind11/complex.h>
 #include <pybind11/functional.h>
 #include <tof.hpp>
+#include <cstdio>
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(limu_py, m) {
-    pybind11::class_<ToF>(m, "ToF")
+    py::class_<ToF>(m, "ToF")
         .def("tof320", &ToF::tof320)
         .def("stopStream", &ToF::stopStream)
         .def("streamDCS", &ToF::streamDCS)
@@ -30,5 +31,29 @@ PYBIND11_MODULE(limu_py, m) {
         .def("setLensCenter", &ToF::setLensCenter)
         .def("getWidth", &ToF::getWidth)
         .def("getHeight", &ToF::getHeight)
+        ;
+    
+    py::class_<Frame>(m, "Frame")
+        .def_readwrite("stride", &Frame::stride)
+        .def_readwrite("dataType", &Frame::dataType)
+        .def_readwrite("width", &Frame::width)
+        .def_readwrite("height", &Frame::height)
+        .def_readwrite("payloadHeaderOffset", &Frame::payloadHeaderOffset)
+        .def_readwrite("px_size;", &Frame::px_size)
+        .def_readwrite("frame_id", &Frame::frame_id)
+        .def_readwrite("distData", &Frame::distData)
+        .def_readwrite("amplData", &Frame::amplData)
+        .def_readwrite("dcsData", &Frame::dcsData)
+        .def_readwrite("n_points", &Frame::n_points)
+        .def_readwrite("data_depth_ptr", &Frame::data_depth)
+        .def_readwrite("data_grayscale_ptr", &Frame::data_grayscale)
+        .def_readwrite("data_amplitude_ptr", &Frame::data_amplitude)
+        .def_readwrite("data_2d_bgr_ptr", &Frame::data_2d_bgr)
+        .def_readwrite("saturated_mask_ptr", &Frame::saturated_mask)
+        .def("get_xyz_rgb", [](Frame &frame) {
+            auto vecsize = frame.n_points * 8;
+            std::vector<float> vec {frame.data_3d_xyz_rgb, frame.data_3d_xyz_rgb + vecsize};
+            return vec;
+        })
         ;
 }
